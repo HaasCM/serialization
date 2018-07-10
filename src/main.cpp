e4b312b0890ec6d77c236e7c34282f23af45e804
Cody Haas <codymhaas@gmail.com>
2018-07-09 21:12:37 -0700

    Add Main Function

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*!
+    \brief File to implement the main function for the template project
+*/
+
+#include "byte_array.hpp"
+#include <istream>
+#include <ostream>
+
+/*!
+    \brief The main function for the template application
+    \param argc the count of arguements, the size of argv
+    \param argv an array of command line arguments for the template project
+    \return an exit code for the application
+ */
+int main(int argc, const char* argv[]) {
+    return 0;
+}
