89921843f60140bfceac753818e15f0c277a6c49
Cody Haas <codymhaas@gmail.com>
2018-09-08 14:54:44 -0700

    Add unit test structure

---
+++ src/tests/byte_array_tests/main.cpp
@@ -0,0 +1,16 @@
+/*!
+    \file main.cpp
+    \brief Main file for the ByteArray unit tests
+*/
+
+#include <cppunit/TestSuite.h>
+
+/*!
+    \brief main function for the ByteArray tests
+    \param argc the amount of command line arguments
+    \param argv an array of command line arguments
+    \return exit code of the application
+*/
+int main(int argc, char* argv[]) {
+
+}
