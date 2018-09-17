55ab5ce19af94487afe6ce13cf44ce354419df6d
Cody Haas <codymhaas@gmail.com>
2018-09-16 18:35:36 -0700

    Add function to test the copy constructor

--- src/tests/byte_array_tests/byte_array_test_suite.cpp
+++ src/tests/byte_array_tests/byte_array_test_suite.cpp
@@ -42,9 +42,30 @@ void ByteArrayTestSuite::test_constructor() {
     CPPUNIT_ASSERT(array.end() - array.begin() == TEST_SIZE);
 }
 
-void ByteArrayTestSuite::test_copyConstructor()
-{
 
+/*!
+    \brief Unit test to test the functionality of the copy constructor
+*/
+void ByteArrayTestSuite::test_copyConstructor() {
+    const char* testData = "1234";
+
+    const int TEST_SIZE = 4;
+    ByteArray array(testData, TEST_SIZE * sizeof(char));
+
+    ByteArray copy(array);
+
+    CPPUNIT_ASSERT(!array.empty() && !copy.empty());
+    CPPUNIT_ASSERT(array.size() == TEST_SIZE && copy.size() == TEST_SIZE);
+    CPPUNIT_ASSERT(array.end() - array.begin() == TEST_SIZE &&
+                   copy.end() - copy.begin() == TEST_SIZE);
+
+    auto copyIter = copy.begin();
+    auto arrayIter = array.begin();
+    while(copyIter != copy.end() && arrayIter != array.end()) {
+        CPPUNIT_ASSERT((*copyIter) == (*arrayIter));
+        copyIter++;
+        arrayIter++;
+    }
 }
 
 void ByteArrayTestSuite::test_append()
