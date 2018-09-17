36bdf5f8741550535bd9d2f0da1b068f269b264f
Cody Haas <codymhaas@gmail.com>
2018-09-16 18:35:51 -0700

    Add function to test the append interface

--- src/tests/byte_array_tests/byte_array_test_suite.cpp
+++ src/tests/byte_array_tests/byte_array_test_suite.cpp
@@ -68,9 +68,58 @@ void ByteArrayTestSuite::test_copyConstructor() {
     }
 }
 
-void ByteArrayTestSuite::test_append()
-{
+/*!
+  \brief Unit test to test the append interface functions
+*/
+void ByteArrayTestSuite::test_append() {
+    std::vector<char> expected;
+    std::string expected_raw = "12341234\0";
+    for(const auto &value : expected_raw) {
+        expected.push_back(value);
+    }
 
+    const char* testData = "1234\0";
+
+    const int TEST_SIZE = 4;
+    ByteArray array(testData, TEST_SIZE * sizeof(char));
+
+    ByteArray copy(array);
+
+    array.append(copy);
+
+    auto checkArray = [&](ByteArray arrayToCheck) {
+        auto expectedIter = expected.begin();
+        auto arrayToCheckIter = arrayToCheck.begin();
+        while(expectedIter != expected.end() && arrayToCheckIter != array.end()) {
+            CPPUNIT_ASSERT((*expectedIter) == (*arrayToCheckIter));
+            expectedIter++;
+            arrayToCheckIter++;
+        }
+    };
+
+    checkArray(array);
+
+    ByteArray arrayTwo(testData, TEST_SIZE * sizeof(char));
+
+    arrayTwo.append(testData, 4);
+
+    checkArray(arrayTwo);
+
+    ByteArray arrayThree(testData, TEST_SIZE * sizeof(char));
+
+    arrayThree.append(testData);
+
+    checkArray(arrayThree);
+
+    ByteArray arrayFour(testData, TEST_SIZE * sizeof(char));
+
+    arrayFour.append("1", 1);
+    arrayFour.append("2", 1);
+    arrayFour.append("3", 1);
+    arrayFour.append("4", 1);
+    arrayFour.append("\0", 1);
+
+    checkArray(arrayFour);
 }
 
 void ByteArrayTestSuite::test_at()
