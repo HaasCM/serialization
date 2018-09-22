cca710466a4513bf9161ec046a3c86b6538d1aa5
Cody Haas <codymhaas@gmail.com>
2018-09-22 10:58:13 -0700

    Stub out stream tests

---
+++ src/tests/byte_stream_tests/byte_stream_test_suite.cpp
@@ -0,0 +1,4 @@
+#include "common.hpp"
+#include "byte_stream_test_suite.hpp"
+
+MAINLESS_TEST(ByteStreamTestSuite)
---
+++ src/tests/byte_stream_tests/byte_stream_test_suite.hpp
@@ -0,0 +1,12 @@
+#ifndef BYTE_STREAM_TEST_SUITE_HPP
+#define BYTE_STREAM_TEST_SUITE_HPP
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class ByteStreamTestSuite : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(ByteStreamTestSuite);
+
+    CPPUNIT_TEST_SUITE_END();
+};
+
+#endif
